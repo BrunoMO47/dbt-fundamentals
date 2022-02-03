0fa004fbda9ef1ee9ea457fce01ec234f1cba779
Bruno <bruno.m.ozaki@gmail.com>
2022-02-03 09:10:25 -0300

    Added packages file and fix to all_dates

--- models/staging/all_dates.sql
+++ models/staging/all_dates.sql
@@ -2,7 +2,7 @@
     materialized = 'table'
 ) }}
 
-{{ dbt.utils.date_spine(
+{{ dbt_utils.date_spine(
     datepart='day',
     start_date = "to_date('01/01/2020', 'mm/dd/yyyy')",
     end_date = "to_date('01/01/2021', 'mm/dd/yyyy')"
